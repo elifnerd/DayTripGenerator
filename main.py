1afb9d0c983f5a714c7978ef700acb75c809e1a2
Elisabeth Ellwanger <elisabethellwanger@MacBook-Pro-22.local>
2023-01-16 16:20:35 -0500

    main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ destination_list = ["Boone", "Asheville", "Wilmington", "Raleigh"]
 
 #Restaurants
 
-restaurant_list = ["random hole-in-the-wall joint", "highest rated on TripAdvisor", "recommendation from locals", "first place you see"]
+restaurant_list = ["Random hole-in-the-wall joint", "Highest rated on TripAdvisor", "Recommendation from locals", "First place you see"]
 
 #Modes of transportation
 
-modes_of_transportation = ["Vespa", "car", "electric scooter", "bicycle"]
+modes_of_transportation = ["Vespa", "Car", "Electric scooter", "Bicycle"]
 
 #Types of entertainment
 
-types_of_entertainment = ["sightseeing", "museums", "breweries", "people watching"]
+types_of_entertainment = ["Sightseeing", "Museums", "Breweries", "People watching"]
 
 day_trip_destination = random.choice(destination_list)
 
@@ -24,4 +24,8 @@ day_trip_transportation = random.choice(modes_of_transportation)
 
 day_trip_entertainment = random.choice(types_of_entertainment)
 
+print("Welcome to your random day trip generator! Let's get started.")
 print(day_trip_destination)
+print(day_trip_transportation)
+print(day_trip_entertainment)
+print(day_trip_restaurant)
